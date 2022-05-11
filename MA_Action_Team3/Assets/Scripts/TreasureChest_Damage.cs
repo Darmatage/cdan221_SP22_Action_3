17cb7110256d38b624e54e918ac37d97d37a310e
JasonWiser <JasonWiserArt@gmail.com>
2022-05-11 13:13:14 -0400

    Treasure chest prefab

---
+++ MA_Action_Team3/Assets/Scripts/TreasureChest_Damage.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class TreasureChest_Damage : MonoBehaviour {
+	private Renderer rend;
+	//public Animator anim;
+	//public GameObject healthLoot;
+	public int maxHealth = 40;
+	public int currentHealth;
+	   
+	public GameObject closedArt;
+	public GameObject openArt;
+
+	public GameObject breakMe;	
+
+	public GameObject treasure;
+
+
+	void Start(){
+		rend = GetComponentInChildren<Renderer> ();
+		//anim = GetComponentInChildren<Animator> ();
+		currentHealth = maxHealth;
+			  
+		closedArt.SetActive(true);
+		openArt.SetActive(false);
+		breakMe.SetActive(false);
+		
+	}
+
+	void OntriggerEnter2D(Collider other){
+		if (other.gameObject.tag == "Player"){breakMe.SetActive(true);}
+	}
+
+	void OntriggerExit2D(Collider other){
+		if (other.gameObject.tag == "Player"){ breakMe.SetActive(false);	}
+	}
+
+	public void TakeDamage(int damage){
+		currentHealth -= damage;
+		rend.material.color = new Color(2.4f, 0.9f, 0.9f, 1f);
+		StartCoroutine(ResetColor());
+		//anim.SetTrigger ("Hurt");
+		if (currentHealth <= 0){
+			Break();
+		}
+	}
+
+       void Break(){
+            //Instantiate (healthLoot, transform.position, Quaternion.identity);
+            //anim.SetBool ("Die", true);
+            GetComponent<Collider2D>().enabled = false;
+			closedArt.SetActive(false);
+			openArt.SetActive(true);
+			
+			Instantiate (treasure, transform.position, Quaternion.identity);
+			  
+              //StartCoroutine(Death());
+       }
+
+       //IEnumerator Death(){
+              //yield return new WaitForSeconds(0.1f);
+              //Debug.Log("You burnt a log");
+              //Destroy(gameObject);
+       //}
+
+       IEnumerator ResetColor(){
+              yield return new WaitForSeconds(0.1f);
+              rend.material.color = Color.white;
+       }
+}
