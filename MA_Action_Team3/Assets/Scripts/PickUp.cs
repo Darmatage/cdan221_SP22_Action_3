20f583805ac007547e3a8333ecc52f02db0b21d8
JasonWiser <JasonWiserArt@gmail.com>
2022-05-11 13:01:10 -0400

    pickup and treasure chest work

--- MA_Action_Team3/Assets/Scripts/PickUp.cs
+++ MA_Action_Team3/Assets/Scripts/PickUp.cs
@@ -4,42 +4,48 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour{
 
-      public GameHandler gameHandler;
-      //public playerVFX playerPowerupVFX;
-      public bool isHealthPickUp = true;
-      public bool isSpeedBoostPickUp = false;
+	public GameHandler gameHandler;
+	//public playerVFX playerPowerupVFX;
+	public bool isHealthPickUp = true;
+	public bool increaseHealthMax =false;
+	public bool isSpeedBoostPickUp = false;
 
-      public int healthBoost = 10;
-      public float speedBoost = 2f;
-      public float speedTime = 2f;
+	public int healthBoost = 10;
+	public float speedBoost = 2f;
+	public float speedTime = 2f;
 
-      void Start(){
+	public int healthMaxIncrease = 10;
+
+	void Start(){
             gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
             //playerPowerupVFX = GameObject.FindWithTag("Player").GetComponent<playerVFX>();
-      }
+	}
 
-      public void OnTriggerEnter2D (Collider2D other){
-            if (other.gameObject.tag == "Player"){
-				Debug.Log("Hit Player");
-                  GetComponent<Collider2D>().enabled = false;
-                  //GetComponent<AudioSource>().Play();
-                  StartCoroutine(DestroyThis());
+	public void OnTriggerEnter2D (Collider2D other){
+		if (other.gameObject.tag == "Player"){
+			Debug.Log("Hit Player");
+			GetComponent<Collider2D>().enabled = false;
+			//GetComponent<AudioSource>().Play();
+			StartCoroutine(DestroyThis());
 
-                  if (isHealthPickUp == true) {
-                        gameHandler.playerGetHit(healthBoost * -1);
-                        //playerPowerupVFX.powerup();
-                  }
+			if (isHealthPickUp == true) {
+				if (increaseHealthMax){
+					gameHandler.StartPlayerHealth += healthMaxIncrease;
+				}
+				gameHandler.playerGetHit(healthBoost * -1);
+				//playerPowerupVFX.powerup();
+			}
 
-                  if (isSpeedBoostPickUp == true) {
-                        other.gameObject.GetComponent<PlayerMove>().speedBoost(speedBoost, speedTime);
-                        //playerPowerupVFX.powerup();
-                  }
-            }
-      }
+			if (isSpeedBoostPickUp == true) {
+				other.gameObject.GetComponent<PlayerMove>().speedBoost(speedBoost, speedTime);
+				//playerPowerupVFX.powerup();
+			}
+		}
+	}
 
-      IEnumerator DestroyThis(){
-            yield return new WaitForSeconds(0.3f);
-            Destroy(gameObject);
-      }
+	IEnumerator DestroyThis(){
+		yield return new WaitForSeconds(0.3f);
+		Destroy(gameObject);
+	}
 
 }
