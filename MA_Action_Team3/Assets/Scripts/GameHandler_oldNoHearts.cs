5cd8b4df0fd52d62ce996ae8cce82dda181b1842
JasonWiser <JasonWiserArt@gmail.com>
2022-08-18 13:29:14 -0400

    Hearts decrease!

--- MA_Action_Team3/Assets/Scripts/GameHandler_oldNoHearts.cs
+++ MA_Action_Team3/Assets/Scripts/GameHandler_oldNoHearts.cs
@@ -5,20 +5,12 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 
-public class GameHandler : MonoBehaviour {
+public class GameHandler_oldNoHearts : MonoBehaviour {
 
 	private GameObject player;
-	public static int playerHearts = 3;
-	public GameObject heart1;
-	public GameObject heart2;
-	public GameObject heart3;
-	public static float heart1fill = 1.0f;
-	public static float heart2fill = 1.0f;
-	public static float heart3fill = 1.0f;
-	
 	public static int playerHealth = 100;
 	public int StartPlayerHealth = 100;
-	//public GameObject healthText;
+	public GameObject healthText;
 	  
 	public static bool haveWisp1 =false;
 	public static bool haveWisp2 =false;
@@ -53,23 +45,15 @@ public class GameHandler : MonoBehaviour {
 
 
     void Start(){
-		player = GameObject.FindWithTag("Player");
-		sceneName = SceneManager.GetActiveScene().name;
-		if (sceneName=="MainMenu"){ //uncomment these two lines when the MainMenu exists
-			playerHealth = StartPlayerHealth;
-		}
-		
-		updateStatsDisplay();
-		pauseMenuUI.SetActive(false);
-		GameisPaused = false;
-		
-		heart1.SetActive(true);
-		heart2.SetActive(true);
-		heart3.SetActive(true);
-		
-		Debug.Log("pHealth = " + playerHealth + ". Hearts = " + playerHearts +  ". h1fill = " + heart1fill + ". h2fill = " + heart2fill + ". h3fill = " + heart3fill );
-               
-	}
+            player = GameObject.FindWithTag("Player");
+            sceneName = SceneManager.GetActiveScene().name;
+            if (sceneName=="MainMenu"){ //uncomment these two lines when the MainMenu exists
+                  playerHealth = StartPlayerHealth;
+            }
+            updateStatsDisplay();
+			 pauseMenuUI.SetActive(false);
+                GameisPaused = false;
+      }
 
 	void Update (){
 		if (Input.GetKeyDown(KeyCode.Escape)){
@@ -107,73 +91,37 @@ public class GameHandler : MonoBehaviour {
             updateStatsDisplay();
     }
 
-	public void playerGetHit(int damage){
-		  
-		if (isDefending == false){
-			playerHealth -= damage;
-			if (playerHealth > 0){
-				//set value of current heart = to playerHealth amount	
-				if (playerHearts == 3){
-					heart3fill = (float)playerHealth / StartPlayerHealth;
-				}
-				else if (playerHearts == 2){
-					heart2fill = (float)playerHealth / StartPlayerHealth;
-				}
-				else if (playerHearts == 1){
-					heart1fill = (float)playerHealth / StartPlayerHealth;
-				}
-				
-				Debug.Log("pHealth = " + playerHealth + ". Hearts = " + playerHearts +  ". h1fill = " + heart1fill + ". h2fill = " + heart2fill + ". h3fill = " + heart3fill );
-                updateStatsDisplay(); // visual of currentHeart: heart3.GetComponent<Image>()
-            }
-			if(damage >= 0){
+      public void playerGetHit(int damage){
+           if (isDefending == false){
+                  playerHealth -= damage;
+                  if (playerHealth >=0){
+                        updateStatsDisplay();
+                  }
+				  if(damage >= 0){
 					// if damage hurts player, play gethit animation
 					//player.GetComponent<PlayerHurt>().playerHit();
-			}
-        }
+				  }
+            }
 
-        if (playerHealth <= 0){
-            playerHealth = 0;
-			playerHearts -= 1;
-			updateStatsDisplay(); // to display # of hearts based on playerHearts counter
-			playerHealth = StartPlayerHealth;
-			
-            if (playerHearts <=0){ 
-				playerDies();
-			}
-        }
+           if (playerHealth > StartPlayerHealth){
+                  playerHealth = StartPlayerHealth;
+				  updateStatsDisplay();
+            }
 
-
-        // if (playerHealth > StartPlayerHealth){
-                  // playerHealth = StartPlayerHealth;
-				  // updateStatsDisplay();
-        // }
-
+           if (playerHealth <= 0){
+                  playerHealth = 0;
+				  updateStatsDisplay();
+                  //playerDies();
+            }
       }
 
-    public void updateStatsDisplay(){
-		if (playerHearts ==3){
-			heart3.GetComponent<Image>().fillAmount = heart3fill;
-		}
-		else if (playerHearts == 2){
-			heart3.SetActive(false);
-			heart2.GetComponent<Image>().fillAmount = heart2fill;
-		}
-		else if (playerHearts == 1){
-			heart2.SetActive(false);
-			heart1.GetComponent<Image>().fillAmount = heart1fill;
-		}
-		else if (playerHearts == 0){
-			heart1.SetActive(false);
-		}
-
-		  
-            //Text healthTextTemp = healthText.GetComponent<Text>();
-            //healthTextTemp.text = "HEALTH: " + playerHealth;
+      public void updateStatsDisplay(){
+            Text healthTextTemp = healthText.GetComponent<Text>();
+            healthTextTemp.text = "HEALTH: " + playerHealth;
 
             Text tokensTextTemp = tokensText.GetComponent<Text>();
             tokensTextTemp.text = "ORBS: " + gotTokens;
-    }
+      }
 
       public void playerDies(){
             //player.GetComponent<PlayerHurt>().playerDead(); // re-add for player controller
@@ -184,7 +132,7 @@ public class GameHandler : MonoBehaviour {
             //player.GetComponent<PlayerMove>().isAlive = false;
             //player.GetComponent<PlayerJump>().isAlive = false;
             yield return new WaitForSeconds(1.0f);
-            SceneManager.LoadScene("EndLose");
+            //SceneManager.LoadScene("EndLose");
       }
 
       public void StartGame() {
